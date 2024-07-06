981d5af262ef651b67413f29185672b1328aab3f
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-07-06 15:55:55 +0530

    created a todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,59 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [names,setNames] = useState([])
-  // const submitHandler = (e)=>{
-  //   e.preventDefault()
-  //   console.log(e.target.name.value)
-  // }
+  const [taskName,setTaskName] =useState("")
+  const [taskDesc,setTaskDesc] = useState("")
+  const [taskStatus,setTaskStatus] = useState("")
+  const [tasks,setTasks] = useState([])
 
   const submitHandler = (e) => {
     e.preventDefault();
-    
-    const copyNames = [...names]
-    copyNames.push(username)
-    setNames(copyNames)
-
-    // setNames([...names,username])
-    
-    setUsername("")
-
-    
-  };
-  console.log(names)
-
-
-  // const changeHandler = (e) => {
-  //   setUsername(e.target.value)
-  // }
+    setTasks([...tasks,{taskName,taskDesc,taskStatus}])
+    setTaskName("")
+    setTaskDesc("")
+    setTaskStatus("")
+  }
+  console.log(tasks)
 
   return (
-    <div>
-      <form onSubmit={submitHandler}>
+    <div className="bg-stone-900 w-full min-h-screen p-2">
+      <h1 className="text-center text-2xl mt-10 text-white">TO DO List</h1>
+      <form onSubmit={submitHandler} className="w-[80%] bg-white m-auto mt-10 rounded-[10px]">
         <input
           type="text"
-          placeholder="name"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
+          placeholder="Task Name"
+          onChange={(e) => setTaskName(e.target.value)}
+          value={taskName}
+          className="w-[30%] p-2 text-xl rounded-[20px]"
         />
-        <button>Submit</button>
+         <input
+          type="text"
+          placeholder="Task Description"
+          onChange={(e) => setTaskDesc(e.target.value)}
+          value={taskDesc}
+          className="w-[30%] p-2 text-xl rounded-[20px]"
+        />
+        <input
+          type="text"
+          placeholder="Task Status"
+          onChange={(e) => setTaskStatus(e.target.value)}
+          value={taskStatus}
+          className="w-[30%] p-2 text-xl rounded-[20px]"
+        />
+
+        <button className="w-[10%] p-2 text-xl rounded-[20px] ">ADD Task</button>
       </form>
+
+      <div className="w-[80%] m-auto mt-10 rounded-[10px]">
+        {tasks.map((task,idx)=>(
+         <div className="flex justify-between w-full bg-white p-2 px-5 text-xl mt-10 rounded-[10px]">
+            <li className=" list-none">
+              {task.taskName} : {task.taskDesc}
+            </li>
+            <li className="list-none">{task.taskStatus}</li>
+         </div>
+        ))}
+      </div>
     </div>
   );
 };
