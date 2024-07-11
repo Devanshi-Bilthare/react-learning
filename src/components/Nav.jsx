8a8a324e0a7ed55f3975fd47f729fe37e11290d0
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-07-11 12:04:55 +0530

    nav link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav className="w-full bg-white p-5 rounded-[20px] flex gap-10">
-          <Link to='/' >Home</Link>
-          <Link to='/create' >Create</Link>
-          <Link to='/show' >Show</Link>
+          <NavLink to='/' className={(e) => e.isActive ? "text-2xl" : ""} >Home</NavLink>
+          <NavLink to='/create' className={(e) => e.isActive ? "text-2xl" : ""}>Create</NavLink>
+          <NavLink to='/show' className={(e) => e.isActive ? "text-2xl" : ""}>Show</NavLink>
         </nav>
   )
 }
